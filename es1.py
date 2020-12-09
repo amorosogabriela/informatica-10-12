f7344c37fd9d492ebd05d06e985a91ec95bb8ed4
amorosogabriela <74314806+amorosogabriela@users.noreply.github.com>
2020-12-09 18:58:58 +0100

    Update es1.py

--- es1.py
+++ es1.py
@@ -1,5 +1,5 @@
 print("parole polindrome")
-stri= input("scrivi una parola e scopri se è polindroma:\n")
+stri = input("scrivi una parola e scopri se è polindroma:\n")
 stri2 = stri[::-1]
 if stri == stri2:
     print("questa parola è polindroma")
